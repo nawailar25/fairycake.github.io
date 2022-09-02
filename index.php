259c069956a9b45741e4e94bf7943ca4fea41b2a
nawailar25 <112732965+nawailar25@users.noreply.github.com>
2022-09-03 05:18:33 +0630

    Add files via upload

---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/all.css">
+    <link rel="stylesheet" href="css/tailwind.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="intro-section">
+        <div class="intro-overlay">
+            <div class="container mx-auto h-full">
+              <div class="w-full md:w-8/12 px-4 md:px-0 mx-auto h-full relative">
+                <div class="flex items-center pt-6 absolute top-0 left-0 w-full relative md:static">
+                    <h2 class="text-2xl text-white">Fairy<span class="font-bold">Cake</span></h2>
+                    <div class="flex-grow"></div>
+                    <ul class="z-50 toggle hidden fixed lg:static top-0 left-0 w-full lg:w-auto h-full bg-white lg:bg-transparent lg:flex items-center text-black lg:text-white list-none font-normal lg:font-bold text-xl lg:text-sm uppercase">
+                        <span class="on-click text-right fas fa-times block lg:hidden pr-4 pt-4"></span>
+                        <a href="baking.html"><li class="text-center pt-12 lg:pt-0 lg:pl-6">baking</li></a>
+                        <a href="decorating.html"><li class="text-center pt-3 lg:pt-0 lg:pl-6">decorating</li></a>
+                        <a href="blog.html"><li class="text-center pt-3 lg:pt-0 lg:pl-6">blog</li></a>
+                        <a href="about.html"><li class="text-center pt-3 md:pt-0 lg:pl-6">about us</li></a>
+                    </ul>
+                    <span class="fas fa-search pl-4 md:pl-6 text-white close-search"></span>
+                    <span class="on-click fas fa-bars lg:hidden text-white pl-6 lg:pl-0"></span>
+                    <div class="z-50 bg-white show-search hidden text-center fixed static top-0 left-0 w-full h-full text-black">
+                        <span class="close-search text-right fas fa-times block pr-4 pt-4"></span>
+                        <div class="flex items-center h-full justify-center">
+                            <form action="" class="">
+                                <input class="py-1 rounded-full px-3 mb-4 md:mb-0 border-solid border-gray-300 bg-gray-300 text-center" type="email" placeholder="Search at fairy cake">
+                                <button class="bg-pink-600 text-white font-bold rounded-full py-1 px-3 text-sm text-center">Search</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex justify-center items-center h-full">
+                    <div class="text-center">
+                        <p class="text-white font-bold text-sm">Baking and decorating supplies</p>
+                        <h1 class="text-white text-4xl font-hairline pb-5">Because life is sweet</h1>
+                        <a href="about.html"><button class="bg-pink-600 text-white font-bold rounded-full py-1 px-3 text-sm">Learn More</button></a>
+                    </div>
+                </div>
+              </div>  
+            </div>
+        </div>
+    </div>
+    <div class="py-20">
+        <div class="container mx-auto">
+            <div class="w-full md:w-1/2 px-4 md:px-0 mx-auto">
+                <p class="text-lg font-thin text-center">Welcome to Fairy Cake - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex neque incidunt eveniet sint, et corrupti ea voluptatum sapiente soluta possimus blanditiis veritatis dolorem labore eum natus mollitia numquam quod aliquam!</p>
+            </div>
+        </div>
+    </div>
+    <div class="pb-24 md:pb-40">
+        <div class="container mx-auto">
+            <div class="w-full lg:w-8/12 px-8 lg:px-0 mx-auto">
+                <h1 class="text-xl md:text-3xl font-bold text-center py-6">Featured Collection</h1>
+                <div class="block md:flex flex-wrap">
+                    <div class="w-full md:w-1/2 lg:w-1/3 px-0 md:px-4">
+                        <div class="img-container ratio-1x1">
+                            <a href="product-detail.html"><img class="rounded-lg" src="image/gem-sugar.jpg" alt=""></a>
+                        </div>
+                        <h2 class="text-xl text-center py-4">Gem sugar -<span class="font-bold">$7</span></h2>
+                    </div>
+                    <div class="w-full md:w-1/2 lg:w-1/3 px-0 md:px-4">
+                        <div class="img-container ratio-1x1">
+                            <a href="product-detail.html"><img class="rounded-lg" src="image/baking-cups.jpg" alt=""></a>
+                        </div>
+                        <h2 class="text-xl text-center py-4">Baking Cup -<span class="font-bold">$4</span></h2>
+                    </div>  
+                    <div class="w-full md:w-1/2 lg:w-1/3 px-0 md:px-4">
+                        <div class="img-container ratio-1x1">
+                            <a href="product-detail.html"><img class="rounded-lg" src="image/gem-pink.jpg" alt=""></a>
+                        </div>
+                        <h2 class="text-xl text-center py-4">Jimmies -<span class="font-bold">$3</span></h2>
+                    </div>  
+                    <div class="w-full md:w-1/2 lg:w-1/3 px-0 md:px-4">
+                        <div class="img-container ratio-1x1">
+                            <a href="product-detail.html"><img class="rounded-lg" src="image/ice-cream.jpg" alt=""></a>
+                        </div>
+                        <h2 class="text-xl text-center py-4">Sugar Tpppers -<span class="font-bold">$9.5</span></h2>
+                    </div>  
+                    <div class="w-full md:w-1/2 lg:w-1/3 px-0 md:px-4">
+                        <div class="img-container ratio-1x1">
+                            <a href="product-detail.html"><img class="rounded-lg" src="image/pearls-pink.jpg" alt=""></a>
+                        </div>
+                        <h2 class="text-xl text-center py-4">Non-Pareils-<span class="font-bold">$4</span></h2>
+                    </div>  
+                    <div class="w-full md:w-1/2 lg:w-1/3 px-0 md:px-4">
+                        <div class="img-container ratio-1x1">
+                            <a href="product-detail.html"><img class="rounded-lg" src="image/pearls-green.jpg" alt=""></a>
+                        </div>
+                        <h2 class="text-xl text-center py-4">Sugar Perals -<span class="font-bold">$4.5</span></h2>
+                    </div>    
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="py-16 bg-green-600">
+        <div class="container mx-auto">
+            <div class="w-full lg:w-8/12 px-8 lg:px-0 mx-auto">
+                <h1 class="text-white text-3xl text-center">Subscribe to our newsletter</h1>
+                <p class="text-white text-sm text-center pt-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <form action="" class="text-center pt-10">
+                    <input class="py-1 rounded-full px-3 mb-4 md:mb-0" type="email" placeholder="Email Address">
+                    <button class="bg-pink-600 text-white font-bold rounded-full py-1 px-3 text-sm">Subscribe</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="py-20 md:py-24">
+        <div class="container mx-auto">
+            <div class="w-full lg:w-8/12 px-8 lg:px-0 mx-auto">
+                <h1 class="text-xl md:text-3xl font-bold text-center pt-6 py-10">Fresh From The Blog</h1>
+                <div class="block md:flex items-center pb-10">
+                    <div class="w-full md:w-1/4">
+                        <div class="img-container ratio-1x1">
+                            <a href="single-blog.html"><img class="rounded-lg pb-1 md:pb-0" src="image/cupcake.jpg" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="w-full md:w-3/4 pl-0 md:pl-6">
+                        <h2 class="text-pink-500 text-lg md:text-xl font-bold">Recip: Cute Cotton Candy Cupcakes</h2>
+                        <p class="pt-2 pb-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga eaque pariatur Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta nulla, tenetur, veniam placeat autem odit reiciendis fugit obcaecati illum quibusdam corporis repellat deleniti maxime dolore quidem, officia natus nostrum architecto. perspiciatis alias quisquam obcaecati suscipit enim dolore accusamus, molestiae vero distinctio. A odio, quis veniam temporibus quasi rem adipisci.</p>
+                        <a href="single-blog.html"><button class="bg-pink-600 text-white font-bold rounded-full py-1 px-3 text-sm">Read More</button></a>
+                    </div>
+                </div>
+                <div class="block md:flex items-center">
+                    <div class="w-full md:w-1/4">
+                        <div class="img-container ratio-1x1">
+                            <a href="single-blog.html"><img class="rounded-lg pb-1 md:pb-0" src="image/ice-cream-2.jpg" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="w-full md:w-3/4 pl-0 md:pl-6">
+                        <h2 class="text-pink-500 text-lg md:text-xl font-bold">Get Crafty! Summmer-Ready Spoons</h2>
+                        <p class="pt-2 pb-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga eaque pariatur Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta nulla, tenetur, veniam placeat autem odit reiciendis fugit obcaecati illum quibusdam corporis repellat deleniti maxime dolore quidem, officia natus nostrum architecto. perspiciatis alias quisquam obcaecati suscipit enim dolore accusamus, molestiae vero distinctio. A odio, quis veniam temporibus quasi rem adipisci.</p>
+                        <a href="single-blog.html"><button class="bg-pink-600 text-white font-bold rounded-full py-1 px-3 text-sm">Read More</button></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="py-24 bg-gray-400">
+        <div class="container mx-auto">
+            <div class="w-full lg:w-8/12 px-8 lg:px-0 mx-auto">
+                <div class="flex items-center flex-wrap">
+                    <div class="w-full md:w-1/3">
+                        <p><span class="fab fa-facebook"></span> Facebook</p>
+                        <p><span class="fab fa-twitter"></span> Twitter</p>
+                        <p><span class="fab fa-pinterest"></span> Pinterest</p>
+                        <p><span class="fab fa-instagram"></span> Instagram</p>
+                    </div>
+                    <div class="w-full md:w-1/3 md:text-center order-last md:order-none">
+                        <h2 class="text-2xl flex-grow">Fairy<span class="font-bold">Cake</span></h2>
+                        <p>Copyright @ 2019 Fairy Cake</p>
+                    </div>
+                    <div class="w-full md:w-1/3 md:text-right py-6 md:py-0">
+                        <p>Privacy</p>
+                        <p>Shipping & Return Policy</p>
+                        <p>Terms Of service</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="js/jquery.js"></script>
+    <script src="js/master.js"></script>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body class="enter">
+    <div class="wrapper">
+        <div class="loginCard">
+            <h1>Welcome</h1>
+            <form action="">
+                <label for="email">E-mail</label>
+                <input type="email" id="email">
+                <label for="password">Password</label>
+                <input type="password" id="password">
+                <a href="index.html"><p class="login-button">Login</p></a>
+                <a href="member.html"><p class="back-button">Back</p></a>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
